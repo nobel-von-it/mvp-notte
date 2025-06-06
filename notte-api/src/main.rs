a6aab679b72084be9ecf1d4e847aa0b834d755a3
nobel-von-it <maksimdavydenko12@gmail.com>
2025-06-06 13:01:29 +0700

    Rework filetype trying recursion

--- notte-api/src/main.rs
+++ notte-api/src/main.rs
@@ -13,59 +13,155 @@ struct OpenRequest {
 
 #[derive(Serialize)]
 enum FileType {
-    File,
-    Dir,
-    Link,
+    File(FileEntry),
+    Dir(DirEntry),
+    Link(LinkEntry),
     Other,
 }
 
 #[derive(Serialize)]
-struct OpenResponse {
+struct FileEntry {
+    name: String,
     path: String,
-    content: Option<Vec<String>>,
-    file_type: FileType,
+
+    content: Vec<String>,
+}
+
+#[derive(Serialize)]
+struct DirEntry {
+    name: String,
+    path: String,
+
+    content: Vec<FileType>,
+}
+
+#[derive(Serialize)]
+struct LinkEntry {
+    name: String,
+    path: String,
+
+    link: FileEntry,
+}
+
+#[derive(Serialize)]
+struct OpenResponse {
+    entry: Option<FileType>,
+}
+
+async fn create_file_entry(path: &PathBuf) -> io::Result<FileEntry> {
+    let abs_path = path.canonicalize().unwrap_or(path.clone());
+    let content = tokio::fs::read_to_string(path).await?;
+
+    Ok(FileEntry {
+        name: path.display().to_string(),
+        path: abs_path.display().to_string(),
+
+        content: content.lines().map(|l| l.to_string()).collect(),
+    })
+}
+
+async fn create_file_entry_with_status(path: &PathBuf) -> (StatusCode, Option<FileEntry>) {
+    let (status, entry) = match create_file_entry(path).await {
+        Ok(e) => (StatusCode::OK, Some(e)),
+        Err(err) => match err.kind() {
+            io::ErrorKind::NotFound => (StatusCode::NOT_FOUND, None),
+            io::ErrorKind::PermissionDenied => (StatusCode::FORBIDDEN, None),
+            _ => (StatusCode::INTERNAL_SERVER_ERROR, None),
+        },
+    };
+    (status, entry)
+}
+
+async fn create_dir_entry(path: &PathBuf) -> io::Result<DirEntry> {
+    let abs_path = path.canonicalize().unwrap_or(path.clone());
+    let mut dir = tokio::fs::read_dir(path).await?;
+
+    let mut content: Vec<FileType> = Vec::new();
+
+    while let Ok(Some(entry)) = dir.next_entry().await {
+        let md = entry.metadata().await?;
+        let file_type = if md.is_dir() {
+            FileType::Dir(create_dir_entry(&entry.path()).await?)
+        } else if md.is_file() {
+            FileType::File(create_file_entry(&entry.path()).await?)
+        } else {
+            FileType::Other
+        };
+        content.push(file_type)
+    }
+
+    Ok(DirEntry {
+        name: path.display().to_string(),
+        path: abs_path.display().to_string(),
+
+        content: content,
+    })
+}
+
+async fn create_dir_entry_with_status(path: &PathBuf) -> (StatusCode, Option<DirEntry>) {
+    let (status, entry) = match create_dir_entry(path).await {
+        Ok(e) => (StatusCode::OK, Some(e)),
+        Err(err) => match err.kind() {
+            io::ErrorKind::NotFound => (StatusCode::NOT_FOUND, None),
+            io::ErrorKind::PermissionDenied => (StatusCode::FORBIDDEN, None),
+            _ => (StatusCode::INTERNAL_SERVER_ERROR, None),
+        },
+    };
+    (status, entry)
+}
+
+async fn create_link_entry(path: &PathBuf) -> io::Result<LinkEntry> {
+    unimplemented!()
+}
+
+async fn create_link_entry_with_status(path: &PathBuf) -> (StatusCode, Option<LinkEntry>) {
+    unimplemented!()
+}
+
+async fn create_file_type(path: &PathBuf) -> (StatusCode, Option<FileType>) {
+    let (status, file_type) = match tokio::fs::metadata(path).await {
+        Ok(md) => {
+            if md.is_file() {
+                let (status, file) = create_file_entry_with_status(path).await;
+                if let Some(file) = file {
+                    (status, Some(FileType::File(file)))
+                } else {
+                    (status, None)
+                }
+            } else if md.is_dir() {
+                let (status, dir) = create_dir_entry_with_status(path).await;
+                if let Some(dir) = dir {
+                    (status, Some(FileType::Dir(dir)))
+                } else {
+                    (status, None)
+                }
+            } else if md.is_symlink() {
+                let (status, link) = create_link_entry_with_status(path).await;
+                if let Some(link) = link {
+                    (status, Some(FileType::Link(link)))
+                } else {
+                    (status, None)
+                }
+            } else {
+                (StatusCode::INTERNAL_SERVER_ERROR, None)
+            }
+        }
+        Err(e) => match e.kind() {
+            io::ErrorKind::NotFound => (StatusCode::NOT_FOUND, None),
+            io::ErrorKind::PermissionDenied => (StatusCode::FORBIDDEN, None),
+            _ => (StatusCode::INTERNAL_SERVER_ERROR, None),
+        },
+    };
+
+    (status, file_type)
 }
 
 async fn api_open(Json(open_request): Json<OpenRequest>) -> ApiReturn<OpenResponse> {
     let path = PathBuf::from(&open_request.path);
 
-    let result = tokio::fs::read_to_string(&path).await;
-    let file_type = match tokio::fs::metadata(&path).await {
-        Ok(md) => {
-            if md.is_file() {
-                FileType::File
-            } else if md.is_dir() {
-                FileType::Dir
-            } else if md.is_symlink() {
-                FileType::Link
-            } else {
-                FileType::Other
-            }
-        }
-        Err(_) => FileType::Other,
-    };
+    let (status, file_type) = create_file_type(&path).await;
 
-    let status = match &result {
-        Err(e) => match e.kind() {
-            io::ErrorKind::NotFound => StatusCode::NOT_FOUND,
-            io::ErrorKind::PermissionDenied => StatusCode::FORBIDDEN,
-            _ => StatusCode::INTERNAL_SERVER_ERROR,
-        },
-        Ok(_) => StatusCode::OK,
-    };
-
-    let content = result
-        .map(|c| Some(c.lines().map(|l| l.to_string()).collect::<Vec<String>>()))
-        .unwrap_or(None);
-
-    (
-        status,
-        Json(OpenResponse {
-            path: path.display().to_string(),
-            content,
-            file_type,
-        }),
-    )
+    (status, Json(OpenResponse { entry: file_type }))
 }
 
 #[derive(Deserialize)]
@@ -75,38 +171,14 @@ struct LsRequest {
 
 #[derive(Serialize)]
 struct LsResponse {
-    path: String,
-    content: Option<Vec<String>>,
+    content: Option<DirEntry>,
 }
 
 async fn api_ls(Json(ls_request): Json<LsRequest>) -> ApiReturn<LsResponse> {
-    let result = tokio::fs::read_dir(&ls_request.path).await;
+    let path = PathBuf::from(&ls_request.path);
+    let (status, dir) = create_dir_entry_with_status(&path).await;
 
-    let (content, status) = match result {
-        Ok(mut dir) => {
-            let mut files = Vec::new();
-            while let Ok(Some(entry)) = dir.next_entry().await {
-                files.push(entry.file_name().to_string_lossy().to_string());
-            }
-            (Some(files), StatusCode::OK)
-        }
-        Err(e) => {
-            let status = match e.kind() {
-                io::ErrorKind::NotFound => StatusCode::NOT_FOUND,
-                io::ErrorKind::PermissionDenied => StatusCode::FORBIDDEN,
-                _ => StatusCode::INTERNAL_SERVER_ERROR,
-            };
-            (None, status)
-        }
-    };
-
-    (
-        status,
-        Json(LsResponse {
-            path: ls_request.path,
-            content,
-        }),
-    )
+    (status, Json(LsResponse { content: dir }))
 }
 
 #[tokio::main]
--- static/script.example.js
+++ static/script.example.js
@@ -3,28 +3,31 @@ document.addEventListener("DOMContentLoaded", async () => {
     const editor = document.getElementById("current-file-content");
 
     async function lsDir(path) {
-        return fetch("http://localhost:8080/api/ls", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                path: path
-            })
-        }).then(response => {
-            response.json().then(data => {
-                return data.content;
-            })
-        }).catch(error => {
-            console.error(error)
-        })
+        try {
+            const response = await fetch("http://localhost:8080/api/ls", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    path: path
+                })
+            });
+            const data = await response.json();
+            return data.content;
+        } catch (error) {
+            console.error(error);
+            return [];
+        }
     }
 
-    function expandDirInExplorer(dirName) {
-        explorer.elements.forEach(element => {
-            if (element.textContent === dirName) {
-                element.classList.add("dir-entry");
-                ls(dirName).forEach(file => {
+    async function expandDirInExplorer(dirName, dirPath) {
+        for (const child of explorer.children) {
+            if (child.textContent.toLowerCase() === dirName.toLowerCase()) {
+                child.classList.add("dir-entry");
+
+                const files = await lsDir(dirPath);
+                for (const file of files) {
                     const li = document.createElement("li");
                     li.classList.add("explorer-entry");
 
@@ -34,32 +37,41 @@ document.addEventListener("DOMContentLoaded", async () => {
                         openEditor(file);
                     })
                     li.appendChild(btn);
-                    element.appendChild(li);
-                })
+                    child.appendChild(li);
+                }
             }
-        })
+        }
     }
 
-    function openEditor(path) {
-        fetch("http://localhost:8080/api/open", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                path: path
-            })
-        }).then(response => {
-            response.json().then(data => {
-                if (data.type === "file") {
-                    editor.value = data.content.join("\n");
-                } else if (data.type === "dir") {
-                    expandDirInExplorer(data.path);
-                }
-            })
-        }).catch(error => {
-            console.error(error)
-        })
+    async function openEditor(path) {
+        try {
+            console.log(path)
+            const response = await fetch("http://localhost:8080/api/open", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    path: path
+                })
+            });
+
+            const json_data = await response.json();
+
+            console.log(json_data);
+
+            const data = json_data.entry;
+
+            if ("File" in data) {
+                editor.value = data.File.content.join("\n");
+            } else if ("Dir" in data) {
+                expandDirInExplorer(data.Dir.name, data.Dir.path);
+            } else {
+                console.error("Unknown file type");
+            }
+        } catch (error) {
+            console.error(error);
+        }
     }
 
     function saveEditor(path) {
@@ -87,7 +99,11 @@ document.addEventListener("DOMContentLoaded", async () => {
     }
 
     const startDir = ".";
-    lsDir(startDir).then(files => files.forEach(file => {
+    const startDirFiles = await lsDir(startDir)
+    console.log(startDirFiles);
+    for (let i = 0; i < startDirFiles.length; i++) {
+        const file = startDirFiles[i];
+
         const li = document.createElement("li");
         li.classList.add("explorer-entry");
 
@@ -99,8 +115,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 
         li.appendChild(btn);
         explorer.appendChild(li);
-    }))
-
+    }
     openEditor("../static/index.example.html");
 })
 
